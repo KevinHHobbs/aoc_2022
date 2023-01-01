e54e2b330d44b7ae47cc7e46fa8b6acb1cd7141f
Kevin H. Hobbs <khhobbs@gmail.com>
2023-01-01 10:29:03 -0500

    2a just parrot back the characters...

--- aoc_2a.cpp
+++ aoc_2a.cpp
@@ -1,24 +1,16 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
 
 int main(int argc, char * argv[]) {
 	std::ifstream input_file;
 	input_file.open( argv[1] );
 	std::string line;
-	size_t total_score = 0;
 	while ( std::getline(input_file, line) ) {
-		std::istringstream iss(line);
-
-		if ( !(iss >> cal) ) {
-			if ( elf_cal > max_cal ) { max_cal = elf_cal; }
-			elf_cal = 0;
-		} else {
-			elf_cal += cal;
-		}
+		char opponent_plays = line[0];
+		char my_play = line[2];
+		std::cout << "them : " << opponent_plays << ", me : " << my_play << std::endl; 
 	}
-	std::cout << max_cal << std::endl;
       	return EXIT_SUCCESS;
 } 
