5178c07b1622772b8c01dbc76ffb435009d50a5a
Kevin H. Hobbs <khhobbs@gmail.com>
2023-01-01 11:03:39 -0500

    2a working

--- aoc_2a.cpp
+++ aoc_2a.cpp
@@ -7,10 +7,30 @@ int main(int argc, char * argv[]) {
 	std::ifstream input_file;
 	input_file.open( argv[1] );
 	std::string line;
+	size_t total_score = 0;
 	while ( std::getline(input_file, line) ) {
 		char opponent_plays = line[0];
 		char my_play = line[2];
-		std::cout << "them : " << opponent_plays << ", me : " << my_play << std::endl; 
+		// Score for my play
+		if ( my_play == 'X' ) { total_score += 1; }
+		if ( my_play == 'Y' ) { total_score += 2; }
+		if ( my_play == 'Z' ) { total_score += 3; }
+
+		// Score for outcome
+		if ( my_play == 'X' && opponent_plays == 'A' ) { total_score += 3; }
+		if ( my_play == 'X' && opponent_plays == 'B' ) { total_score += 0; }
+		if ( my_play == 'X' && opponent_plays == 'C' ) { total_score += 6; }
+
+		if ( my_play == 'Y' && opponent_plays == 'A' ) { total_score += 6; }
+		if ( my_play == 'Y' && opponent_plays == 'B' ) { total_score += 3; }
+		if ( my_play == 'Y' && opponent_plays == 'C' ) { total_score += 0; }
+
+		if ( my_play == 'Z' && opponent_plays == 'A' ) { total_score += 0; }
+		if ( my_play == 'Z' && opponent_plays == 'B' ) { total_score += 6; }
+		if ( my_play == 'Z' && opponent_plays == 'C' ) { total_score += 3; }
 	}
+
+	std::cout << total_score << std::endl;
+
       	return EXIT_SUCCESS;
 } 
