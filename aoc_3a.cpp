196706c1720ee46f7bbf3fe2068422d71f76450e
Kevin H. Hobbs <hobbsk@ohio.edu>
2023-01-01 16:45:19 -0500

    3a done

--- aoc_3a.cpp
+++ aoc_3a.cpp
@@ -9,6 +9,7 @@ int main(int argc, char * argv[]) {
 	std::ifstream input_file;
 	input_file.open( argv[1] );
 	std::string line;
+  size_t total = 0;
 	while ( std::getline(input_file, line) ) {
     std::set<char> A (line.begin(), line.begin() + line.size() / 2 );
     std::set<char> B (line.begin() + line.size() / 2, line.end() );
@@ -18,9 +19,11 @@ int main(int argc, char * argv[]) {
       B.begin(), B.end(),
       std::inserter(I, I.begin())
     );
-    std::cout << *I.begin() << std::endl;
+    char intersection = *I.begin();
+    if ( 'a' <= intersection && intersection <= 'z') { total +=  1 + intersection - 'a'; }
+    if ( 'A' <= intersection && intersection <= 'Z') { total += 27 + intersection - 'A'; }
 	}
 
-
+  std::cout << total << std::endl;
   return EXIT_SUCCESS;
 } 
