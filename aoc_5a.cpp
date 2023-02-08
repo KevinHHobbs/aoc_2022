c9d9fc2b3a2fed507d9c47b508cc47d4a3b2e819
Kevin H. Hobbs <khhobbs@gmail.com>
2023-02-08 08:31:22 -0500

    starting on 5a

--- aoc_5a.cpp
+++ aoc_5a.cpp
@@ -4,13 +4,32 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <vector>
+
+
+typedef std::stack  <char>           CargoStackType;
+typedef std::vector <CargoStackType> CargoArrangementType;
+
+CargoArrangementType read_cargo_arrangement(
+        std::ifstream &input_file,
+        std::string  &line,
+        CargoArrangementType &cargo_arrangement
+        ) {
+    if (line.empty()) {
+        return cargo_arrangement; // should read next section
+    }
+
+    while ( std::getline(input_file, line) ) {
+    }
+    return cargo_arrangement;
+}
+
 
 int main(int argc, char * argv[]) {
+
     std::ifstream input_file;
     input_file.open( argv[1] );
     std::string line;
-    while ( std::getline(input_file, line) ) {
-    }
 
     return EXIT_SUCCESS;
 } 
